9cd68028bd952fc3fdcc28d616dfa5d07f58a2ee
Carlos Ferreira <carlosferreira@outlook.com>
2018-12-11 19:39:19 +0000

    User Interaction: Keyboard input

---
+++ User Interaction/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int number;
+
+    std::cout << "Enter an integer: ";
+    std::cin >> number;
+
+    int squared = number * number;
+
+    std::cout << "The squared value of " << number << " is " << squared << std::endl;
+
+    return 0;
+}
