6e1457019f447dc129adbd8298a261f7537acaaf
Carlos Ferreira <carlosferreira@outlook.com>
2018-12-11 19:02:28 +0000

    Chapter 1: std::setfill()

--- Chapter 1/main.cpp
+++ Chapter 1/main.cpp
@@ -3,11 +3,11 @@
 
 int main ()
 {
-	int year = 2018, mon = 12, day = 11;
+	int year = 2018, mon = 12, day = 5;
 
 	std::cout << "Today is: "
-		<< std::setw(2) << day << "/"
-		<< std::setw(2) << mon << "/"
+		<< std::setw(2) << std::setfill('0') << day << "/"
+		<< std::setw(2) << std::setfill('0') << mon << "/"
 		<< year << std::endl;
 
 	return 0;
