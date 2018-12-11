ea1309714616914ccf1f5f4403e88637cdb03996
Carlos Ferreira <carlosferreira@outlook.com>
2018-12-11 18:41:52 +0000

    Chapter 1: Formatting of text using std::setw

---
+++ Chapter 1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+
+int main ()
+{
+	std::cout << std::setw(5) << 2018 << " is today's year." << std::endl;
+	std::cout << std::setw(5) << 12 << " is today's month." << std::endl;
+	std::cout << std::setw(5) << 11 << " is today's day." << std::endl;
+
+	return 0;
+}
