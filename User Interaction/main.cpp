4c36067d9bb8b16670048d9a0b7c93433feea19f
Carlos Ferreira <carlosferreira@outlook.com>
2018-12-13 20:16:18 +0000

    User Interaction: Constant variables

--- User Interaction/main.cpp
+++ User Interaction/main.cpp
@@ -2,19 +2,10 @@
 
 int main()
 {
-    int n1, n2, n3, n4, n5;
+    int x = 5;
+    x = 10;
 
-    std::cout << "Enter five integers, separated by space: ";
-
-    std::cin >> n1 >> n2 >> n3 >> n4 >> n5;
-
-    int sum = n1 + n2 + n3 + n4 + n5;
-
-    float average = sum / 5.0f;
-
-    std::cout << "The sum is " << sum << std::endl;
-
-    std::cout << "The average is " << average;
+    const int y = 3;
 
     return 0;
 }
